3232b606eee242e5fb6b36f400b0c07e79268e8f
Alex Canario <alexcanario@gmail.com>
2024-08-01 11:21:24 -0300

    refactor: angular project create

--- 02-essencials/src/app/app.component.ts
+++ 02-essencials/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
