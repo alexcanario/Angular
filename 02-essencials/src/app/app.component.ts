68d21abeac4196e93a15ed08c068ddd4cf3a005b
Alex Canario <alexcanario@gmail.com>
2024-08-01 15:45:24 -0300

    feat(component): add header new component

--- 02-essencials/src/app/app.component.ts
+++ 02-essencials/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component'
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = '02-essencials';
---
+++ 02-essencials/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    // template: '<h1>Hello World</h1>',   
+    standalone: true,
+    templateUrl: './header.component.html',
+})
+
+export class HeaderComponent {
+
+}
