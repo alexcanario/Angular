c2268fb0fd730366252dfd90ae27e4a949c04d09
Alex Canario <alexcanario@gmail.com>
2024-08-02 00:23:18 -0300

    feat[sidebar]: add msg to console when btn clicked

--- 02-essencials/src/app/user/user.component.ts
+++ 02-essencials/src/app/user/user.component.ts
@@ -12,4 +12,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex]
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    console.log("Clicked!")
+  }
+
 }
