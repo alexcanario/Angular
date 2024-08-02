4fa926b4d7aca0dc64f2c1aa47902ed2cb2b20dd
Alex Canario <alexcanario@gmail.com>
2024-08-02 00:41:00 -0300

    refactor[sidebar]: onclick button event

--- 02-essencials/src/app/user/user.component.ts
+++ 02-essencials/src/app/user/user.component.ts
@@ -10,15 +10,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
+  selectedUser = DUMMY_USERS[randomIndex]  
 
   get imagePath() {
     return 'assets/users/' + this.selectedUser.avatar;
   }
-
+  
   onSelectUser() {
-    console.log("Clicked!")
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex]  
   }
 
 }
