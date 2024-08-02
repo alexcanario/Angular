aa66fa7c7a51a051f242f8af38a52687aef42d22
Alex Canario <alexcanario@gmail.com>
2024-08-02 00:06:50 -0300

    feat:[menu]: add user data to sidebar button

--- 02-essencials/src/app/user/user.component.ts
+++ 02-essencials/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -8,5 +11,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomIndex]
 }
