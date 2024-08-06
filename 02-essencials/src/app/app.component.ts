1cf607d65d37ce7f5734f8ab6269e1a6df4c1155
Alex Canario <alexcanario@gmail.com>
2024-08-05 22:03:00 -0300

    feat[user]: added data to user task buttons

--- 02-essencials/src/app/app.component.ts
+++ 02-essencials/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,5 @@ import { UserComponent } from './user/user.component';
 })
 export class AppComponent {
   title = '02-essencials';
+    users = DUMMY_USERS;
 }
