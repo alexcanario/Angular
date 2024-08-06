7929eeb0476f8f7d4ccab1d839911a46ca044c64
Alex Canario <alexcanario@gmail.com>
2024-08-05 22:34:56 -0300

    feat[user]: added signal commented to monitoring

--- 02-essencials/src/app/user/user.component.ts
+++ 02-essencials/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';  //without signal
+// import { Component, computed, input } from '@angular/core'; //with signal
 
 @Component({
   selector: 'app-user',
@@ -9,9 +10,22 @@ import { Component, Input } from '@angular/core';
 })
 
 export class UserComponent {
+
+  //without signal
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
 
+
+  //with signal
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  //with signal
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  //without signal
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
