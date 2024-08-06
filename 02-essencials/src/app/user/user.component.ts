3680d6c35dffc7bc4d063160c41a46f3f994e30e
Alex Canario <alexcanario@gmail.com>
2024-08-05 22:03:00 -0300

    feat[user]: added data to user task buttons

--- 02-essencials/src/app/user/user.component.ts
+++ 02-essencials/src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,15 +9,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]  
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
-  
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex]  
+    return 'assets/users/' + this.avatar;
   }
 
+  onSelectUser() { }
 }
